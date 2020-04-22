f991dc8f5b8afc20baa6574f99cc859b86a39571
Bryan-Rdz <rdza485@gmail.com>
2020-04-22 15:52:49 -0500

    Commit inicial 10

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // FIRMA SECRETA DE JWT 
 process.env.FIRMA = process.env.FIRMA || 'firma-super-secreta';
 
+// EXPIRACION DEL TOKEN
+process.env.EXPTIME = process.env.EXPTIME || '3h';
+
 // CONEXION A LA BASE DE DATOS 
 let urlDB;
 
